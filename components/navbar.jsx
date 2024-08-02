7aeca02e0cc54a4bace1ca6e9bb3a9e3ba638b80
Fahad <38499959+fahd07gh@users.noreply.github.com>
2024-08-02 19:59:36 +0000

    fix navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,5 +1,5 @@
-"use client";
-import { useState, useCallback, useEffect } from "react";
+// "use client";
+// import { useState, useCallback, useEffect } from "react";
 import Link from "next/link";
 import LanguageButton from "./languageButton";
 import { ThemsToggle } from "./ThemsToggle";
@@ -9,37 +9,38 @@ export default function navbar({ lang }) {
     en: ["Home", "About", "Project", "Contact"],
     ar: ["الرئيسية", "عني", "مشروع", "تواصل"],
   };
-  const [y, setY] = useState(0);
-  const [show, setShow] = useState(true);
-  const handleNavigation = useCallback(
-    (e) => {
-      const window = e.currentTarget;
-      if (y > window.scrollY) {
-        // console.log("scrolling up");
-        setShow(true);
-      } else if (y < window.scrollY) {
-        // console.log("scrolling down");
-        setShow(false);
-      }
-      setY(window.scrollY);
-    },
-    [y]
-  );
+  // const [y, setY] = useState(0);
+  // const [show, setShow] = useState(true);
+  // const handleNavigation = useCallback(
+  //   (e) => {
+  //     const window = e.currentTarget;
+  //     if (y > window.scrollY) {
+  //       // console.log("scrolling up");
+  //       setShow(true);
+  //     } else if (y < window.scrollY) {
+  //       // console.log("scrolling down");
+  //       setShow(false);
+  //     }
+  //     setY(window.scrollY);
+  //   },
+  //   [y]
+  // );
 
-  useEffect(() => {
-    setY(window.scrollY);
-    window.addEventListener("scroll", handleNavigation);
+  // useEffect(() => {
+  //   setY(window.scrollY);
+  //   window.addEventListener("scroll", handleNavigation);
 
-    return () => {
-      window.removeEventListener("scroll", handleNavigation);
-    };
-  }, [handleNavigation]);
+  //   return () => {
+  //     window.removeEventListener("scroll", handleNavigation);
+  //   };
+  // }, [handleNavigation]);
 
   return (
     <header className="flex items-center justify-center gap-6 fixed top-5 left-0 w-full z-50">
       <nav className="max-w-[400px] w-fitt h-10 bg-secondary border flex items-center justify-between gap-3 px-3 rounded-full delay-150 duration-100">
         <div
-          className={`w-full flex flex-nowrap justify-start items-center text-xl h-full ${!show ? "hidden" : ""}`}
+          className={`w-full flex flex-nowrap justify-start items-center text-xl h-full`}
+          // ${!show ? "hidden" : ""}
         >
           {navLinks[lang]?.map((link, index) => (
             <Link
